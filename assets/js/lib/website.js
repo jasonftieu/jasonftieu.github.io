fb031f3ccdb67ddc242c16ac71289257d52613e6
Jason Tieu <jtieu@Jasons-MacBook-Pro.local>
2017-12-26 16:23:13 -0600

    added transitions to website

---
+++ assets/js/lib/website.js
@@ -0,0 +1,102 @@
+window.sr = ScrollReveal();
+/* JT Logo */
+sr.reveal('#jtlogo', {
+  delay: 500,
+  duration: 2000,
+  easing: 'cubic-bezier(0.4, 0.2, 0.1, 1)',
+});
+/*Navbar*/
+sr.reveal('#collapsable-nav', {
+  delay: 500,
+  duration: 2000,
+  easing: 'cubic-bezier(0.4, 0.2, 0.1, 1)',
+});
+/*Hello, my name is jason tieu*/
+sr.reveal('#hello', {
+  delay: 500,
+  duration: 2000,
+  easing: 'cubic-bezier(0.4, 0.2, 0.1, 1)',
+});
+/* I am an aspiring software engineer */
+sr.reveal('#coverDescription', {
+  delay: 1500,
+  duration: 2000,
+  easing: 'cubic-bezier(0.4, 0.2, 0.1, 1)',
+});
+/* Learn more */
+sr.reveal('#introLink', {
+  delay: 2000,
+  duration: 2000,
+  easing: 'cubic-bezier(0.4, 0.2, 0.1, 1)',
+});
+/* Jason Tieu is a junior ... */
+sr.reveal('#introContent', {
+  delay: 200,
+  duration: 2500,
+  easing: 'cubic-bezier(0.4, 0.2, 0.1, 1)',
+  viewfactor: 0.2,
+});
+/* My Projects */
+sr.reveal('#projectHeader', {
+  delay: 500,
+  duration: 2500,
+  easing: 'cubic-bezier(0.4, 0.2, 0.1, 1)',
+  viewfactor: 0.2,
+});
+/* Card 1*/
+sr.reveal('#card1', {
+  delay: 500,
+  duration: 2500,
+  easing: 'cubic-bezier(0.4, 0.2, 0.1, 1)',
+  viewfactor: 0.2,
+});
+/* Card 2 */
+sr.reveal('#card2', {
+  delay: 500,
+  duration: 2500,
+  easing: 'cubic-bezier(0.4, 0.2, 0.1, 1)',
+  viewfactor: 0.2,
+});
+/* Card 3 */
+sr.reveal('#card3', {
+  delay: 500,
+  duration: 2500,
+  easing: 'cubic-bezier(0.4, 0.2, 0.1, 1)',
+  viewfactor: 0.2,
+});
+/* My Skills */
+sr.reveal('#skillsHeader', {
+  delay: 500,
+  duration: 2500,
+  easing: 'cubic-bezier(0.4, 0.2, 0.1, 1)',
+  viewfactor: 0.2,
+});
+/* Skills */
+sr.reveal('#skills', {
+  delay: 1000,
+  duration: 2500,
+  easing: 'cubic-bezier(0.4, 0.2, 0.1, 1)',
+  viewfactor: 0.2,
+});
+/* Contact Me */
+sr.reveal('#contactme', {
+  delay: 700,
+  duration: 2500,
+  easing: 'cubic-bezier(0.4, 0.2, 0.1, 1)',
+  viewfactor: 0.2,
+});
+/* contact me photo */
+sr.reveal('#contactmePhoto', {
+  delay: 500,
+  duration: 2500,
+  origin: 'left',
+  distance: '400px',
+  viewfactor: 0.2,
+});
+/* contact me links */
+sr.reveal('.contactmeLinks', {
+  delay: 700,
+  duration: 2500,
+  easing: 'cubic-bezier(0.4, 0.2, 0.1, 1)',
+  viewfactor: 0.2,
+});
